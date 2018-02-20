c99800f0fed1a382c10e36bffa29d71cacf78adb
Ciarán Coffey <github@ccoffey.ie>
2018-02-20 13:09:23 +0000

    Lines breaks don't play well with curl

--- set_external_ip.cloudflare.sh
+++ set_external_ip.cloudflare.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 #  Set the external IP on cloudflare dns, dyndns style,
 #  using the current external (ipv4) address.
 #
@@ -12,14 +13,16 @@
 #
 #  More details available here:
 #  https://api.cloudflare.com/#getting-started-requests
-DNS_ID=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID\
-	/dns_records?name=$TARGET_DNS" \
-	-H "Content-Type:application/json" \
-	-H "X-Auth-Key:$X_AUTH_KEY" \
-	-H "X-Auth-Email:$X_AUTH_EMAIL" \ | jq '.["result"][0]["id"]' -r)
+DNS_ID=$(curl -X GET\
+ "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?name=$TARGET_DNS"\
+ -H "Content-Type:application/json"\
+ -H "X-Auth-Key:$X_AUTH_KEY"\
+ -H "X-Auth-Email:$X_AUTH_EMAIL"\
+ | jq '.["result"][0]["id"]' -r)
 EXTERNAL_IP=$(curl httpbin.org/ip | jq '.["origin"]' -r)
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID\
-	/dns_records/$DNS_ID" -H "Content-Type:application/json"\ 
-	-H "X-Auth-Key:$X_AUTH_KEY"\ -H "X-Auth-Email:$X_AUTH_EMAIL" \
-        --data '{"type":"A","name":"'$TARGET_DNS'",\
-	"content":"'$EXTERNAL_IP'","ttl":120,"proxied":false}'
+curl -X PUT\
+  "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_ID"\
+  -H "Content-Type:application/json"\
+  -H "X-Auth-Key:$X_AUTH_KEY"\
+  -H "X-Auth-Email:$X_AUTH_EMAIL"\
+  --data '{"type":"A","name":"'$TARGET_DNS'","content":"'$EXTERNAL_IP'","ttl":120,"proxied":false}'
